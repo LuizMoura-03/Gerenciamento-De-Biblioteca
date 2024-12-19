49aad578ff96cb87bd2e780fd2c8754bdb16ce16
LuizFernando <barbosa.luiz@zup.com.br>
2024-12-19 15:32:00 -0300

    :construction: Adding methods add book and list borrowed books.

--- src/biblioteca/Usuario.java
+++ src/biblioteca/Usuario.java
@@ -1,4 +1,26 @@
 package biblioteca;
 
+import java.util.ArrayList;
+
 public class Usuario {
+    private String nome;
+    private int id;
+    private ArrayList<Livro> livrosEmprestados;
+
+    public Usuario(String nome, int id, ArrayList<Livro> livrosEmprestados) {
+        this.nome = nome;
+        this.id = id;
+        this.livrosEmprestados = livrosEmprestados;
+    }
+
+    public void adicionarLivro(Livro livro) {
+        livrosEmprestados.add(livro);
+        System.out.println("Livro adicionado na lista de emprestimo.");
+    }
+
+    public void listarLivrosEmprestados() {
+        System.out.println("Livros emprestados por " + nome + ":");
+        for (Livro livro : livrosEmprestados) {
+        }
+    }
 }
